4775bdff9c9523ae91c03c797e9d94c53e76e53c
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-08-17 11:36:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Welcome to Flatbook, ';
+const welcomeMessage = currentUser + '!';
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + '!';
+let shortGreeting = "Welcome, " + '!';
+// shortGreeting = currentUser + '!';
